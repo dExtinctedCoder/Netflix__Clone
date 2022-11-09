c10ed628b5037e3af32d519db503dd389d07303c
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-09 14:00:32 +0100

    folder for all assets dependecies(.jpg, .gif, .png...)

---
+++ src/assets/data/links.js
@@ -0,0 +1,79 @@
+
+
+export const links = [
+  {
+    id: 'a&li1',
+    href: 'https://help.netflix.com/support/412',
+    subText: 'FAQ',
+  },
+  {
+    id: 'a&li2',
+    href: 'https://help.netflix.com/',
+    subText: 'Help Center',
+  },
+  {
+    id: 'a&li3',
+    href: 'https://www.netflix.com/youraccount',
+    subText: 'Account',
+  },
+  {
+    id: 'a&li4',
+    href: 'https://media.netflix.com/',
+    subText: 'Media Center',
+  },
+  {
+    id: 'a&li5',
+    href: 'http://ir.netflix.com/',
+    subText: 'Investor Relations',
+  },
+  {
+    id: 'a&li6',
+    href: 'https://jobs.netflix.com/jobs',
+    subText: 'Jobs',
+  },
+  {
+    id: 'a&li7',
+    href: 'https://www.netflix.com/watch',
+    subText: 'Ways to Watch',
+  },
+  {
+    id: 'a&li8',
+    href: 'https://help.netflix.com/legal/termsofuse',
+    subText: 'Terms of Use',
+  },
+  {
+    id: 'a&li9',
+    href: 'https://help.netflix.com/legal/privacy',
+    subText: 'Privacy',
+  },
+  {
+    id: 'a&li10',
+    href: 'https://www.netflix.com/ng/',
+    subText: 'Cookie Preferences',
+  },
+  {
+    id: 'a&li11',
+    href: 'https://help.netflix.com/legal/corpinfo',
+    subText: 'Corporate Information',
+  },
+  {
+    id: 'a&li12',
+    href: 'https://help.netflix.com/contactus',
+    subText: 'Contact Us',
+  },
+  {
+    id: 'a&li13',
+    href: 'https://fast.com/',
+    subText: 'Speed Test',
+  },
+  {
+    id: 'a&li14',
+    href: 'https://help.netflix.com/legal/notices',
+    subText: 'Legal Notices',
+  },
+  {
+    id: 'a&li15',
+    href: 'https://www.netflix.com/ng/browse/genre/839338',
+    subText: 'Only on Netflix',
+  },
+]
