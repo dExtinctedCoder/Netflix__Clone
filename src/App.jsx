17a33522487e971719b008e2852cac55faabcf56
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-09 13:51:29 +0100

    Create routes for page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Login from './pages/Login'
+import Error from './pages/Error'
 
 function App() {
 
   return (
     <div className="page__control">
-      Netflix Clone App
+      <Routes>
+        <Route path="/" element={<Landing />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="*" element={<Error />}/>
+      </Routes>
     </div>
   )
 }
