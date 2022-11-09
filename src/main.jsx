c8fa1e8ed137b4436be0907e52d0546d5df4f97e
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-09 13:53:01 +0100

    Restructure file for router-dom configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Route } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Route>
+      <App />
+    </Route>
   </React.StrictMode>
 )
